518de4dc2ac11668dfbc993fcffa1f1faa6fc03f
Kubajsa <Kubajsa@users.noreply.github.com>
2021-02-07 21:26:52 +0100

    Made the move check less strict

--- src/main/java/me/jakub/randomtp/listeners/CountdownEvents.java
+++ src/main/java/me/jakub/randomtp/listeners/CountdownEvents.java
@@ -1,6 +1,7 @@
 package me.jakub.randomtp.listeners;
 
 import me.jakub.randomtp.utils.TeleportUtils;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +13,17 @@ public class CountdownEvents implements Listener {
     public void onMove(PlayerMoveEvent e){
         Player player = e.getPlayer();
         if (TeleportUtils.willTp.contains(player) && TeleportUtils.hasCountdown.contains(player)){
+            Location loc1 = e.getFrom();
+            Location loc2 = e.getTo();
+            if (    Math.round(loc1.getX()) == Math.round(loc2.getX()) &&
+                    Math.round(loc1.getY()) == Math.round(loc2.getY()) &&
+                    Math.round(loc1.getZ()) == Math.round(loc2.getZ())){
 
-            TeleportUtils.willTp.remove(player);
-            player.sendMessage("§4Cancelled teleportation!");
+            }else {
+
+                TeleportUtils.willTp.remove(player);
+                player.sendMessage("§4Cancelled teleportation!");
+            }
         }
     }
 
