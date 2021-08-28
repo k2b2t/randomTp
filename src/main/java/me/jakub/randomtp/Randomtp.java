e85939e7fe356f8b9c8ba4fb18c069be2f681774
Kubajsa <71178261+Kubajsa@users.noreply.github.com>
2021-08-28 11:06:35 +0200

    Bump to 2.19.5

--- src/main/java/me/jakub/randomtp/Randomtp.java
+++ src/main/java/me/jakub/randomtp/Randomtp.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 
 public final class Randomtp extends JavaPlugin {
 
-    public static final String VERSION = "2.19.4";
+    public static final String VERSION = "2.19.5";
 
     private static Economy econ = null;
 
